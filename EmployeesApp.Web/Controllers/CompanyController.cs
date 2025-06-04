786ecd1c46bb7a406b8e29057de88638ace18d58
davarng <113583854+davarng@users.noreply.github.com>
2025-06-04 15:35:55 +0200

    CompanyController

---
+++ EmployeesApp.Web/Controllers/CompanyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeesApp.Web.Controllers
+{
+    public class CompanyController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+    }
+}
